b133911d6731bc00f3101187e39d0b6f0586de89
michaelfehr <michael_fehrgmx.de>
2022-11-08 13:26:08 +0100

    updated

--- app/src/main/java/de/androidcrypto/abhiandroidfragmentexample/SecondFragment.java
+++ app/src/main/java/de/androidcrypto/abhiandroidfragmentexample/SecondFragment.java
@@ -1,12 +1,13 @@
 package de.androidcrypto.abhiandroidfragmentexample;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 
 public class SecondFragment extends Fragment {
